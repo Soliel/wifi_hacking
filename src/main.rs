19ea05dde352b78c264c21c541e2bb4323c8b43b
Rachel Clark <Solcode@outlook.com>
2024-03-20 17:40:21 -0400

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+#![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+#![feature(const_trait_impl)]
+#![allow(incomplete_features)]
+
+mod sensors;
+
+use cyw43_pio::PioSpi;
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_rp::bind_interrupts;
+use embassy_rp::gpio::{Output, Level};
+use embassy_rp::peripherals::{DMA_CH0, PIN_23, PIN_25, PIO0, I2C0};
+use embassy_rp::pio::{ InterruptHandler as PioInterruptHandler, Pio };
+use embassy_rp::i2c::{self, Config, InterruptHandler as I2CInterruptHandler};
+use embassy_time::Timer;
+use static_cell::make_static;
+use {defmt_rtt as _, panic_probe as _};
+use crate::sensors::tlv493d::TLV493D;
+
+bind_interrupts!(struct Irqs {
+    PIO0_IRQ_0 => PioInterruptHandler<PIO0>;
+    I2C0_IRQ => I2CInterruptHandler<I2C0>;
+});
+
+
+
+
+#[embassy_executor::task]
+async fn wifi_task(runner: cyw43::Runner<'static, Output<'static, PIN_23>, PioSpi<'static, PIN_25, PIO0, 0, DMA_CH0>>) -> ! {
+    runner.run().await
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    info!("Hello World!");
+
+    //Inits clock (default at 12MHz), DMA, GPIOs, and Timers
+    let rp2040 = embassy_rp::init(Default::default());
+    let fw = include_bytes!("../Firmware/43439A0.bin");
+    let clm = include_bytes!("../Firmware/43439A0_clm.bin");
+
+    // Let's setup the CYW43 Wifi chip
+    // the pico uses GPIO 23, 24, 25, and 29 for WiFi
+    let pwr = Output::new(rp2040.PIN_23, Level::Low); // This is WL_ON for the pico W and maps to PIN 35 for the RP 2040
+    let cs = Output::new(rp2040.PIN_25, Level::High); // This is WL_CS for the pico W and maps to PIN 37 for the RP 2040
+    // The RP2040 has 2 PIO cores, each with 4 state machines that can be used to program for extra hardware.
+    // These PIO cores operate separately from the CPU and communicate via FIFO buffers and Interrupt Requests
+    let mut pio = Pio::new(rp2040.PIO0, Irqs); // Initialize our PIO core.
+    // This sets up an SPI interface via PIO. Which leaves the RP2040's 2 SPI interfaces available.
+    let spi = PioSpi::new(&mut pio.common, pio.sm0, pio.irq0, cs, rp2040.PIN_24, rp2040.PIN_29, rp2040.DMA_CH0);
+
+    let state = make_static!(cyw43::State::new());
+    let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
+    unwrap!(spawner.spawn(wifi_task(runner)));
+
+    control.init(clm).await;
+    control.set_power_management(cyw43::PowerManagementMode::PowerSave).await;
+    control.gpio_set(0, true).await;
+
+    let sda = rp2040.PIN_16;
+    let scl = rp2040.PIN_17;
+
+    let i2c = i2c::I2c::new_async(rp2040.I2C0, scl, sda, Irqs, Config::default());
+    const TLV493D_ADDR: u16 = 0xBD;
+
+    let mut sensor = TLV493D::new(i2c, TLV493D_ADDR);
+    sensor.init().await;
+
+    loop {
+        let vals = sensor.get_sensor_reading().await;
+        println!("{:?}", vals);
+        Timer::after_millis(50).await;
+    }
+}
---
+++ src/sensors/tlv493d.rs
@@ -0,0 +1,178 @@
+use defmt::{Format, info, trace};
+use {defmt_rtt as _, panic_probe as _};
+use embassy_rp::i2c::{Async, I2c, Instance};
+
+pub struct TLV493D<'d, T: Instance> {
+    i2c_addr: u16,
+    read_buffer: [u8; 10],
+    write_buffer: [u8; 4],
+    last_frm: u8,
+    pub i2c_dev: I2c<'d, T, Async>
+}
+
+#[derive(Format)]
+pub struct MagnetVals {
+    x: f32,
+    y: f32,
+    z: f32
+}
+
+pub struct ReadMask {
+    byte_num: usize,
+    byte_mask: u8,
+    byte_shift: u8
+}
+
+impl ReadMask {
+    const fn from(vals: (i32, i32, i32)) -> Self {
+        Self {
+            byte_num: vals.0 as usize,
+            byte_mask: vals.1 as u8,
+            byte_shift: vals.2 as u8
+        }
+    }
+}
+
+pub struct WriteMask {
+    byte_num: usize,
+    byte_mask: u8,
+    byte_shift: u8
+}
+
+impl WriteMask {
+    const fn from(vals: (i32, i32, i32)) -> Self {
+        Self {
+            byte_num: vals.0 as usize,
+            byte_mask: vals.1 as u8,
+            byte_shift: vals.2 as u8
+        }
+    }
+}
+
+impl const From<(i32, i32, i32)> for ReadMask {
+    fn from(value: (i32, i32, i32)) -> Self {
+        ReadMask {
+            byte_num: value.0 as usize,
+            byte_mask: value.1 as u8,
+            byte_shift: value.2 as u8
+        }
+    }
+}
+
+impl const From<(i32, i32, i32)> for WriteMask {
+    fn from(value: (i32, i32, i32)) -> Self {
+        WriteMask {
+            byte_num: value.0 as usize,
+            byte_mask: value.1 as u8,
+            byte_shift: value.2 as u8
+        }
+    }
+}
+
+// Read Masks
+pub const BX0:   ReadMask = ReadMask::from((0, 0xFF, 0));
+pub const BX1:   ReadMask = ReadMask::from((4, 0xF0, 4));
+pub const BY0:   ReadMask = ReadMask::from((1, 0xFF, 0));
+pub const BY1:   ReadMask = ReadMask::from((4, 0x0F, 0));
+pub const BZ0:   ReadMask = ReadMask::from((2, 0xFF, 0));
+pub const BZ1:   ReadMask = ReadMask::from((5, 0x0F, 0));
+pub const TEMP:  ReadMask = ReadMask::from((3, 0xF0, 4));
+pub const TEMP2: ReadMask = ReadMask::from((6, 0xFF, 0));
+pub const FRM:   ReadMask = ReadMask::from((3, 0x0C, 2));
+pub const CH:    ReadMask = ReadMask::from((3, 0x03, 0));
+pub const POWERDOWNFLAG: ReadMask = ReadMask::from((5, 0x10, 4));
+pub const READRES1: ReadMask = ReadMask::from((7, 0x18, 3));
+pub const READRES2: ReadMask = ReadMask::from((8, 0xFF, 0));
+pub const READRES3: ReadMask = ReadMask::from((9, 0x1F, 0));
+
+// Write Masks
+pub const PARITY: WriteMask = WriteMask::from((1, 0x80, 7));
+pub const ADDR: WriteMask = WriteMask::from((1, 0x60, 5));
+pub const INT: WriteMask = WriteMask::from((1, 0x04, 2));
+pub const FAST: WriteMask = WriteMask::from((1, 0x02, 1));
+pub const LOWPOWER: WriteMask = WriteMask::from((1, 0x01, 0));
+pub const TEMP_DISABLE: WriteMask = WriteMask::from((3, 0x80, 7));
+pub const LP_PERIOD: WriteMask = WriteMask::from((3, 0x40, 6));
+pub const POWERDOWN: WriteMask = WriteMask::from((3, 0x20, 5));
+pub const WRITERES1: WriteMask = WriteMask::from((1, 0x18, 3));
+pub const WRITERES2: WriteMask = WriteMask::from((2, 0xFF, 0));
+pub const WRITERES3: WriteMask = WriteMask::from((3, 0x1F, 0));
+
+impl<'d, T: Instance> TLV493D<'d, T> {
+    pub fn new(i2c_dev: I2c<'d, T, Async>, addr: u16) -> Self {
+        TLV493D {
+            i2c_addr: addr,
+            last_frm: 0,
+            read_buffer: [0; 10],
+            write_buffer: [0; 4],
+            i2c_dev,
+        }
+    }
+
+    pub async fn init(&mut self) {
+        self.setup_write_buffer().await;
+        self.set_write_data(ADDR, 0);
+        self.set_write_data(PARITY, 1);
+        self.set_write_data(FAST, 1);
+        self.set_write_data(LOWPOWER, 1);
+        self.write_device().await;
+    }
+
+    /// set_write_data will set a specific value in the write buffer. It gets rid of all the bit bashing.
+    pub fn set_write_data(&mut self, msg_type: WriteMask, value: u8) {
+        let mut current_byte = self.write_buffer[msg_type.byte_num];
+        current_byte &= !msg_type.byte_mask;
+        current_byte |= value << msg_type.byte_shift;
+        self.write_buffer[msg_type.byte_num] = current_byte;
+    }
+
+    /// get_read_data will isolate a specific value from the Read buffer and pull it out.
+    pub fn get_read_data(&mut self, msg_type: ReadMask) -> u8 {
+        let raw_read_val = self.read_buffer[msg_type.byte_num];
+        (raw_read_val & msg_type.byte_mask) >> msg_type.byte_shift
+    }
+
+    pub async fn read_device(&mut self) {
+        self.i2c_dev.read_async(self.i2c_addr, &mut self.read_buffer).await.unwrap();
+        info!("Read data: {:X}", self.read_buffer);
+    }
+
+    pub async fn write_device(&mut self) {
+        info!("Write Data: {:X}", self.write_buffer);
+        self.i2c_dev.write_async(self.i2c_addr, self.write_buffer).await.unwrap();
+    }
+
+    async fn setup_write_buffer(&mut self) {
+        self.read_device().await;
+        let res1 = self.get_read_data(READRES1);
+        let res2 = self.get_read_data(READRES2);
+        let res3 = self.get_read_data(READRES3);
+
+        self.set_write_data(WRITERES1, res1);
+        self.set_write_data(WRITERES2, res2);
+        self.set_write_data(WRITERES3, res3);
+    }
+
+    pub async fn get_sensor_reading(&mut self) -> MagnetVals {
+        self.read_device().await;
+        let xTop = self.get_read_data(BX0);
+        let yTop = self.get_read_data(BY0);
+        let zTop = self.get_read_data(BZ0);
+        let xBot = self.get_read_data(BX1);
+        let yBot = self.get_read_data(BY1);
+        let zBot = self.get_read_data(BZ1);
+        info!("raw bytes: \n\tX-TOP: {:X}, X-BOT: {:X}\n\tY-TOP: {:X}, Y-BOT: {:X}\n\tZ-TOP: {:X}, Z-BOT: {:X}",
+            xTop, xBot, yTop, yBot, zTop, zBot);
+
+        let x = (self.get_read_data(BX0) as i8 as i16) << 4 | self.get_read_data(BX1) as i16;
+        let y = (self.get_read_data(BY0) as i8 as i16) << 4 | self.get_read_data(BY1) as i16;
+        let z = (self.get_read_data(BZ0) as i8 as i16) << 4 | self.get_read_data(BZ1) as i16;
+        info!("vals no convert: X: {}, Y: {}, Z: {}", x, y, z);
+
+        MagnetVals {
+            x: x as f32 * 0.098f32,
+            y: y as f32 * 0.098f32,
+            z: z as f32 * 0.098f32,
+        }
+    }
+}
